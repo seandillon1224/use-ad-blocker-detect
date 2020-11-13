3d9662d37f3c0590eda5106dc7b5711bc5c2475c
Sean Dillon <seandillon1224@gmail.com>
2020-11-12 21:59:22 -0500

    Edit docs and clean up types and hook return values

--- example/modal.tsx
+++ example/modal.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { useIsAdBlocker } from '../.';
 import './Modal.module.css';
 
+const someFunc = () => console.log('called');
+
 const Modal = () => {
-  const [showModal, storageSetter] = useIsAdBlocker({storageType: 'session'});
+  const {showComponent, storageSetter} = useIsAdBlocker({storageType: 'session', hasBlockerCb: someFunc});
   const [isOpen, setIsOpen] = React.useState<boolean>(true);
 
   const handleClose = () => {
@@ -11,7 +13,7 @@ const Modal = () => {
     setIsOpen(false);
   };
 
-  return showModal && isOpen ? (
+  return showComponent && isOpen ? (
     <div className="modal">
       <div className="modalMain">
         <h1>PLEASE UNBLOCK ME !!!</h1>
